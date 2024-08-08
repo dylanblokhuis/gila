5a528821f8a7a8886260f5b8a9b42d413095091b
Dylan Blokhuis <dylanblokhuis3@hotmail.com>
2024-08-08 23:39:40 +0200

    remove comments

--- examples/triangle.zig
+++ examples/triangle.zig
@@ -30,24 +30,6 @@ const Triangle = [_]Vertex{
 };
 
 pub fn main() !void {
-    // const bytes = try slang.compileToSpv("hello-world.slang", "computeMain", .SLANG_STAGE_COMPUTE);
-    // std.debug.print("{any}", .{bytes});
-
-    // std.debug.print("{s}\n", .{c.slang.spGetBuildTagString()});
-
-    // const session = c.slang.spCreateSession();
-    // defer c.slang.spDestroySession(session);
-
-    // const request = c.slang.spCreateCompileRequest(session);
-    // defer c.slang.spDestroyCompileRequest(request);
-
-    // const target_index = c.slang.spAddCodeGenTarget(request, .SLANG_SPIRV);
-    // const profile = c.slang.spFindProfile(session, "glsl_450".ptr);
-    // c.slang.spSetTargetProfile(request, target_index, profile);
-
-    // const target_index2 = c.slang.spAddCodeGenTarget(request, .SLANG_DXIL);
-    // std.debug.print("{d} {d}\n", .{ target_index, target_index2 });
-
     _ = c.glfwSetErrorCallback(errorCallback);
     if (c.glfwInit() == 0) {
         std.debug.panic("failed to initialize GLFW", .{});
